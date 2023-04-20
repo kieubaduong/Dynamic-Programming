bcb1e772e26d33a586a6f859de5b50cc7b10b0d8
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-04-20 13:48:55 +0700

    add damn java code

--- bytelandian_gold_coins/Main.java
+++ bytelandian_gold_coins/Main.java
@@ -1,12 +1,24 @@
 package bytelandian_gold_coins;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
-public class Main{
-    HashMap<Long, Long> dp = new HashMap<Long, Long>();
+public class Main {
+    private static HashMap<Long, Long> dp = new HashMap<Long, Long>();
 
-    
-    public static void main(String[] args){
+    public static long maxCoin(long n) {
+        if (!dp.containsKey(n)) {
+            dp.put(n, Math.max(n, maxCoin(n / 2) + maxCoin(n / 3) + maxCoin(n / 4)));
+        }
+        return dp.get(n);
+    }
 
+    public static void main(String[] args) {
+        dp.put(0L, 0L);
+        try (Scanner sc = new Scanner(System.in)) {
+            while (sc.hasNextLong()) {
+                System.out.println(maxCoin(sc.nextLong()));
+            }
+        }
     }
 }
